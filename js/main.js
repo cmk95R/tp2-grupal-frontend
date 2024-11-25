1ca3ca9a05bf0b2cc0a3bfd9f80d2720d096e7ba
Jean <micheljeanpierre647@gmail.com>
2024-11-25 00:34:52 -0300

    toda la wea funcional

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@
     - Se valorará un código limpio, bien comentado, separado por comentarios en bloques, etc 
 */
 
-
+let cuadriculaProductos = document.querySelector(".product-grid");
+let barraBusqueda = document.querySelector(".search-bar");
+let botonesCarrito = document.querySelector(".add-to-cart");
+let objetosCarrito = document.querySelector(".cart-items");
+let precioCarrito = document.querySelector(".total-prize");
+let contadoCarrito = document.querySelector(".cart-count");
 
 
 /*  PREGUNTA 1_____________
@@ -34,8 +39,43 @@
     ______________________
 
     Escriban acá su razonamiento y explicación de todo tu proceso
+
+    1)  Crear un objeto grupo con los datos del equipo (nombre y miembros).
+        Utilizar console.log para imprimirlo en consola.
+        Seleccionar el elemento .nombreGrupo con querySelector y actualizar su contenido con innerHTML o textContent.
+
+    2)  Necesitamos un array que almacene los productos de la tienda. Cada producto será representado como un objeto con 
+        propiedades como id, nombre, precio y img para simplificar su uso en las funciones posteriores
+
+    3)  Iterar el array con forEach.
+        Generar dinámicamente elementos HTML (div, img, p) para cada producto.
+        Integrar esta funcionalidad en una función inicializadora.
+    
+    4)  Capturar el evento keyup en el input de búsqueda.
+        Comparar el valor del input con los nombres del array usando filter.
+        Actualizar dinámicamente la lista de productos mostrados.
+
+    5)  Crear un array carrito para almacenar los productos seleccionados.
+        Añadir un botón "Agregar al carrito" para cada producto.
+        Escuchar el clic (onclick) en este botón y agregar el producto al array.
+        Mostrar los productos del carrito en la sección carrito.
+
+    6)  nada xd
 */
 
+
+/* 1. Almacenar nuestros datos en un objeto e imprimir el nombre del grupo en la consola y en el nav:
+    <div class="nombreGrupo"></div>*/
+const grupo = {
+    nombre: "Grupo 2",
+    integrantes: ["Jean Pierre Michel Kong", "Cristian Velazquez", "Roberto Barragan", "William Romero"]
+};
+
+// Mostrar el nombre del grupo en el nav
+document.querySelector('.nombreGrupo').textContent = grupo.nombre;
+
+// Mostrar el nombre del grupo en la consola
+console.log(grupo.nombre);
 
 
 
@@ -62,45 +102,12 @@ let productosFruteria = [
 ];
 
 
-
-
 /*  PREGUNTA 3_____________
     Agreguen a la funcion inicializadora init() una función para imprimir nombre del grupo en el nav y en la consola.
 */
-// 1: Almacenar muestros nombres
-const grupo = {
-    nombre: "Grupo 2",
-    integrantes: ["Jean Pierre Michel Kong", "Cristian Velazquez", "Roberto Barragan", "William Romero"]
-};
-
-// Mostrar el nombre del grupo en el nav
-document.querySelector('.nombreGrupo').textContent = grupo.nombre;
-
-// Mostrar el nombre del grupo en la consola
-console.log(grupo.nombre);
-
-
-/*  PREGUNTA 4_____________
-    Creen una función para imprimir en pantalla los productos del array de objetos y agreguenla a la funcion inicializadora
-    El html que deben agregar debe tener el siguiente esquema (para que se apliquen los estilos)
-
-        <div class="product-card">
-            <img src="" alt="">
-            <h3></h3>
-            <p>$</p>
-            <button class="add-to-cart">Agregar a carrito</button>
-        </div>
-*/
-
-let cuadriculaProductos = document.querySelector(".product-grid");
-let barraBusqueda = document.querySelector(".search-bar");
-let botonesCarrito = document.querySelector(".add-to-cart");
-let objetosCarrito = document.querySelector(".cart-items");
-let precioCarrito = document.querySelector(".total-prize");
-let contadoCarrito = document.querySelector(".cart-count");
-
-let carrito = [];
-
+function init() {
+    mostrarProductos(productosFruteria);
+}
 
 function mostrarProductos(array) {
     let cartaProducto = "";
@@ -119,10 +126,7 @@ function mostrarProductos(array) {
 }
 
 
-
-/*  OPCIONAL / PREGUNTA 5_____________
-    Escriban una función filtro, por ejemplo, asociada a un evento keyup, que recoja los valores del campo input y ejecute con cada evento un filtro que actualice los productos
-*/
+/* 4. OPCIONAL 1 / Realizar una función filtro que mediante un evento como keyup recoja los datos del input y filtre los productos que contengan esos valores */
 
 function filtroProductos() {
     const keywords = barraBusqueda.value.toLowerCase();
@@ -133,21 +137,8 @@ function filtroProductos() {
 }
 
 
-
-
-/*  OPCIONAL / PREGUNTAS 6 y 7_____________
-
-    1. Elaboren la funcionalidad de carrito. Agreguen funcionalidad al boton de cada producto para introducir ese elemento en un contenedor de carrito e imprimirlo en el listado con id "cart-items"" del HTML
-
-    El HTML que deben agregar debe seguir el siguiente esquema (para que se apliquen los estilos)
-
-    <li class="item-block">
-        <p class="item-name">nombreproducto - $precioproducto</p>
-        <button class="delete-button">Eliminar</button>
-    </li>
-
-    2. Se valorará que se almacenen los productos del carrito en un localStorage
-*/
+/* 5. OPCIONAL 2 / Realizar la funcionalidad de carrito */
+let carrito = [];
 
 function agregarCarrito (id){
     console.log(`id del producto: ${id}`);
@@ -176,13 +167,9 @@ function mostrarCarrito() {
     objetosCarrito.innerHTML = carritoCompra;
 }
 
+/* 6. OPCIONAL 3 / Hacer que esa memoria sea persistente guardando los elementos del carrito en localStorage */
+
 
 // Funcion inicializadora
-
-function init() {
-        mostrarProductos(productosFruteria);
-}
-
 init();
 barraBusqueda.addEventListener("keyup", filtroProductos);
-
